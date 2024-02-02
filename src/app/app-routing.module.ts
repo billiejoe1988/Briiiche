912092f1dcea595690ca7dc3335564985d26142c
MauriArbelaiz <mauricio.arbelaiz@gmail.com>
2024-02-02 18:43:05 -0300

    FIX, style.css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './layouts/auth/pages/login/login.component';
 import { NotFoundModuleComponent } from './layouts/not-found-module/not-found-module.component';
-import { DashboardRoutingModule } from './layouts/dashboard/dashboard-routing.module';
 
 const routes: Routes = [
   { path: 'auth/login', component: LoginComponent },
   { path: '404', component: NotFoundModuleComponent },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, 
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { 
+    path: 'dashboard', 
+    loadChildren: () => import('./layouts/dashboard/dashboard.module').then(m => m.DashboardModule) 
+  },
   { path: '**', redirectTo: '/404' }, 
 ];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes),
-    DashboardRoutingModule, 
   ],
   exports: [RouterModule],
 })
